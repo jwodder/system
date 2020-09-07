8fae78e0f50494292b86fe5701a073ab8aafaf1a
John T. Wodder II <git@varonathe.org>
2020-09-07 18:33:56 +0000

    That filter isn't needed.

--- plugins/filter_plugins/util.py
+++
@@ -1,6 +0,0 @@
-class FilterModule(object):
-    def filters(self):
-        return {
-            # Just `str.splitlines` doesn't support unicode arguments
-            "splitlines": lambda s: s.splitlines(),
-        }
